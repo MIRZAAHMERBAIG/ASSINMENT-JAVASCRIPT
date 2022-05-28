ee9e7a0ee55208d308d92a85a82f94864a54aeeb
MIRZA AHMER BAIG <mahmerbaig3@gmail.com>
2022-05-28 23:01:40 +0500

    Update app.js
    chapter 5 question no 7 till done

--- app.js
+++ app.js
@@ -121,19 +121,108 @@ var sense = "sensitve"
 document.write("Variables names are case" + " " + sense +"<br />");
 var key = "keywords"
 document.write("Variables names should not be JS" + " " + key +"<br />");
+document.write("<h2> CHAPTER 5 </h2>");
 
+                            //  CHAPTER 5
+// QUESTION NO 1(a)
+var a=3;
+var b=7;
+var c=a+b
+document.write("SUM OF " +a +" AND "+b +" IS "+ c +"<br />");
 
+// QUESTION NO 2(A)
+var e=77;
+var f=7;
+var d=e-f;
+document.write("SUBTRACTION OF " +e +" AND "+f +" IS "+ d +"<br />");
 
+// QUESTION NO 2(B)
+var g=77;
+var h=7;
+var i=g*h;
+document.write("SUBTRACTION OF " +g +" AND "+h +" IS "+ i +"<br />");
 
+// QUESTION NO 2(C)
+var j=50;
+var k=5;
+var l=j/k;
+document.write("SUBTRACTION OF " +j +" AND "+k +" IS "+ l +"<br />" +"<br />");
 
+// QUESTION NO 3
+var shooter;
+document.write("VALUE AFTER VARAIBLE DECLARATION IS : " + shooter +"<br />");
+shooter=5;
+document.write(" INTIAL VALUE IS : " + shooter +"<br />");
+++shooter
+document.write(" VALUE AFTER INCREMENT IS : " + shooter +"<br />");
+shooter=shooter+7;
+document.write(" VALUE AFTER ADDITION IS : " + shooter +"<br />");
+--shooter;
+document.write(" VALUE AFTER DECREMENT IS : " + shooter +"<br />");
+shooter=shooter%shooter;
+document.write(" THE REMAINDER IS : " + shooter +"<br />");
 
+// QUESTION NO 4
+var price=600;
+var totalCost=price*5;
+document.write("<h2> TOTAL COST TO BUY 5 MOVIE TICKET IS " + totalCost + "PKR" + "</h2>");
 
-
-
-
-
-
-
-
-
-    
+// QUESTION NO 5
+var tableNumber=+prompt("ENTER YOUR TABLE NUMBER",4);
+var now=1;
+var result=tableNumber*now
+document.write("<h2> TABLE OF THE NUMBER IS :" + tableNumber + "</h2>");
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+document.write(tableNumber  + " * " + now + " = " + result + "<br />" );
+++now
+result=tableNumber*now
+// QUESTION NO 5 FINISHED
+// QUESTION NO 6
+document.write("<h2> THE TEMPERATURE CONVERTER "  + "</h2>");
+var celciusTemperature=+prompt("ENTER THE TEMPERATURE WHICH YOU WANT TO CONVERT\NCENTIGRADE INTO FARENHITE",25);
+var farenhiteTemperature=+prompt("ENTER THE TEMPERATURE WHICH YOU WANT TO CONVERT\NFARENHITE INTO CENTIGRADE",70);
+var conversion1= (farenhiteTemperature-32)*5/9;
+var conversion2= (celciusTemperature*9/5)+32;
+document.write(celciusTemperature+"C IS "+ conversion2+"F"+ "<br />")
+document.write(farenhiteTemperature+"F IS "+ conversion1+"C"+ "<br />");
+// QUESTION NO 6 FINISHED
+// QUESTION NO 7
+var priceOFitem1 =700;
+var priceOFitem2 =700;
+var orderedQuantityofItem1 =1;
+var orderedQuantityofItem2 =1;
+var shippingcharges=100;
+var totalshop=priceOFitem1+priceOFitem2+shippingcharges;
+document.write("<h2> THE SHOPPING CART "  + "</h2>");
+document.write("THE PRICE OF ITEM 1 IS  "  +  priceOFitem1 + "<br />");
+document.write("THE PRICE OF ITEM 2 IS  "  +  priceOFitem2 + "<br />");
+document.write("THE QUANTITY OF ITEM 1 IS"+ orderedQuantityofItem1  + "<br />");
+document.write("THE QUANTITY OF ITEM 2 IS"+ orderedQuantityofItem2  + "<br />" + "<br />");
+document.write("<h5> TOTAL COST OF YOUR ORDER IS "  + totalshop +"</h5>");
+// QUESTION NO 7 FINISHED
