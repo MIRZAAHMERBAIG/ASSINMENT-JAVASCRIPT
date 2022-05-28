76d5c9699268834bf64f3e8b7b22fe34b015fbaf
MIRZA AHMER BAIG <mahmerbaig3@gmail.com>
2022-05-28 20:29:02 +0500

    CHP 1 TO 4 DONE
    ASSINMENT OF CHAPTER 1 TO 4 IS DONE

---
+++ app.js
@@ -0,0 +1,139 @@
+                                   
+//                         //  CHAPTER NO 1  
+// document.write("<h2> CHAPTER 1 </h2>");
+
+// // question no 1
+// alert("Hello MIRZA AHMER BAIG");
+
+// // question no 2
+// alert("Error! Please Enter A Valid Password");
+
+// // question no 3
+// alert("Welcome To JS land \n Happy Coding!");
+
+// // question no 4
+// alert("Welcome To JS land");
+// alert("Happy Coding!");
+
+// // question no 5
+// alert("Hello... I can run JS through my web browser's console");
+
+
+//                                     // Chapter no 2
+// document.write("<h2> CHAPTER 2 </h2>");
+
+
+// // question no 1
+// var  useranme
+
+// // question no 2
+// var myname = "mirza AHMER BAIG"
+
+// // question no 3
+
+// var message = "Hello world"
+// alert(message);
+
+// // question no 4
+
+// var name = "Jhone Doe"
+// alert(name);
+
+// var age = "15 years old"
+// alert(age);
+
+// var certificate = "Certified Mobile Application Development"
+// alert(certificate);
+
+// // question no 5
+
+// var PIZZA = "PIZZA"
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+
+// // question no 6
+
+// var email = " example@example.com"
+// alert("My email is" +  email); 
+
+// // question no 7
+
+// var book = " A smarter way to learn JavaScript"
+// alert("I am trying to learn the book" + book);
+
+// // question no 8
+// document.write("yah! i can write HTML through JS");
+// // question no 9
+// var input = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬"
+// alert(input);
+
+
+
+                            //   CHAPTER NO 3
+document.write("<h2> CHAPTER 3 </h2>");
+
+// QUESTION NO 1
+var age ="19"
+alert("I AM " + age + " YEARS OLD");
+
+// QUESTION NO 2
+var visitor ="15"
+alert("YOU HAVE VISITED THIS SITE " + visitor + " TIMES");
+
+// QUESTION NO 3
+var birthYear =2003;
+document.write("MY BIRTH YEAR IS " + birthYear +"<br />" );
+document.write("DATA TYPE OF MY DECLARED VARAIBLE IS  NUMBER" +"<br />" +"<br />" );
+
+// QUESTION NO 4
+var storeNAME="CHASE UP SHOPPING CENTER";
+var visitorNAME="MIRZA AHMER BAIG";
+var productTITLE="T SHIRT";
+var quantity=9;
+document.write(visitorNAME +"ORDERED "+ quantity + productTITLE +"ON"+ storeNAME + "." +"<br />" +"<br />"  );
+document.write("<h2> CHAPTER 4 </h2>");
+                         
+                        //    CHAPTER NO 4
+// QUESTION NO 1 
+var a,b,c;
+
+// QUESTION NO 2
+// LEGAL VARAIBLE
+var myname;
+var my_name;
+var myName;
+var $;
+var varaible;
+// ILLEGAL VARAIBLE
+// var 123;
+// var my name;
+// var you&i;
+// var alert;
+// var if;
+
+// QUESTION NO 3
+document.write("<h2> RULE FOR NAMING JS VARAIBLE </h2>");
+var num="numbers";
+var sign="$";
+var example="my_1stVariable";
+document.write("VARAIBLE NAMES CAN ONLY CONTAIN " + num + ", "+ sign +" AND _ . FOR EXAMPLE :"+ example +"<br />");
+var sense = "sensitve"
+document.write("Variables names are case" + " " + sense +"<br />");
+var key = "keywords"
+document.write("Variables names should not be JS" + " " + key +"<br />");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
